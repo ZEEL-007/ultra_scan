cdc48316ed7b2695740d0c0b1822ac72a7b15c6b
ZEEL KOTADIYA <kotadiyazeel007@gmail.com>
2025-07-26 13:05:24 +0530

    Update ultra_scan.sh

--- ultra_scan.sh
+++ ultra_scan.sh
@@ -58,6 +58,49 @@ function show_header() {
 # Run dependency check
 check_dependencies
 
+# Function to handle scan and optional output
+function run_scan() {
+    local scan_cmd="$1"
+    local target="$2"
+
+    read -p "Do you want to save the output? (y/n): " save_choice
+    if [[ "$save_choice" =~ ^[Yy]$ ]]; then
+        read -p "Enter desired output format (txt, html, xml, json, gnmap): " format
+        folder="$(dirname "$0")/results/$target"
+        mkdir -p "$folder"
+        case "$format" in
+            txt)
+                eval "$scan_cmd" > "$folder/output.txt"
+                echo -e "\e[1;32mSaved as $folder/output.txt\e[0m"
+                ;;
+            html)
+                eval "$scan_cmd" -oX "$folder/output.xml"
+                xsltproc "$folder/output.xml" -o "$folder/output.html"
+                echo -e "\e[1;32mSaved as $folder/output.html\e[0m"
+                ;;
+            xml)
+                eval "$scan_cmd" -oX "$folder/output.xml"
+                echo -e "\e[1;32mSaved as $folder/output.xml\e[0m"
+                ;;
+            json)
+                eval "$scan_cmd" -oX "$folder/output.xml"
+                xml2json < "$folder/output.xml" > "$folder/output.json"
+                echo -e "\e[1;32mSaved as $folder/output.json\e[0m"
+                ;;
+            gnmap)
+                eval "$scan_cmd" -oG "$folder/output.gnmap"
+                echo -e "\e[1;32mSaved as $folder/output.gnmap\e[0m"
+                ;;
+            *)
+                echo -e "\e[1;31mUnsupported format. Running scan without saving.\e[0m"
+                eval "$scan_cmd"
+                ;;
+        esac
+    else
+        eval "$scan_cmd"
+    fi
+}
+
 # Main Loop
 while true; do
     show_header
@@ -103,128 +146,41 @@ while true; do
     read opt
 
     case $opt in
-        1)
-            read -p "Enter target: " target
-            nmap -sn "$target"
-            ;;
-        2)
-            read -p "Enter target: " target
-            nmap -sS "$target"
-            ;;
-        3)
-            read -p "Enter target: " target
-            nmap -sU "$target"
-            ;;
-        4)
-            read -p "Enter target: " target
-            nmap -O "$target"
-            ;;
-        5)
-            read -p "Enter target: " target
-            nmap -sV "$target"
-            ;;
-        6)
-            read -p "Enter target: " target
-            nmap -A "$target"
-            ;;
-        7)
-            read -p "Enter target: " target
-            nmap -F "$target"
-            ;;
-        8)
-            read -p "Enter target: " target
-            nmap -p- "$target"
-            ;;
-        9)
-            read -p "Enter target: " target
-            nmap --top-ports 100 "$target"
-            ;;
-        10)
-            read -p "Enter target: " target
-            nmap -f --data-length 200 "$target"
-            ;;
-        11)
-            read -p "Enter target: " target
-            nmap --script vuln "$target"
-            ;;
-        12)
-            read -p "Enter target: " target
-            nmap --script default,vuln "$target"
-            ;;
-        13)
-            read -p "Enter target: " target
-            read -p "Enter decoy IP (or RND:10): " decoy
-            nmap -D "$decoy" "$target"
-            ;;
-        14)
-            read -p "Enter target: " target
-            nmap --traceroute "$target"
-            ;;
-        15)
-            read -p "Enter target: " target
-            read -p "Enter ports (e.g. 21,22,80): " ports
-            nmap -p "$ports" "$target"
-            ;;
-        16)
-            read -p "Enter target: " target
-            read -p "Output filename (no extension): " filename
-            nmap -A -oA "$filename" "$target"
-            ;;
-        17)
-            read -p "Enter target: " target
-            read -p "Enter MAC type (e.g. 0=random): " mac
-            nmap --spoof-mac "$mac" "$target"
-            ;;
-        18)
-            read -p "Enter domain: " target
-            nmap --script dns-brute "$target"
-            ;;
-        19)
-            read -p "Enter target: " target
-            nmap --script http-title "$target"
-            ;;
-        20)
-            read -p "Enter target: " target
-            nmap --script smb-enum-shares,smb-enum-users "$target"
-            ;;
-        21)
-            read -p "Enter target: " target
-            nmap --script ftp-anon "$target"
-            ;;
-        22)
-            read -p "Enter target: " target
-            nmap --script ssl-cert "$target"
-            ;;
-        23)
-            read -p "Enter target: " target
-            nmap --script http-waf-detect "$target"
-            ;;
-        24)
-            read -p "Enter target: " target
-            nmap --script http-methods "$target"
-            ;;
-        25)
-            read -p "Enter target: " target
-            nmap --script http-robots.txt "$target"
-            ;;
-        26)
-            read -p "Enter target: " target
-            nmap --script whois "$target"
-            ;;
-        27)
-            read -p "Enter target: " target
-            nmap --script safe "$target"
-            ;;
-        28)
-            echo -e "\n\e[1;31mExiting Ultra Scan... Goodbye!\e[0m"
-            exit 0
-            ;;
-        *)
-            echo -e "\e[1;31mInvalid option. Try again.\e[0m"
-            ;;
+        1)  read -p "Enter target: " target; run_scan "nmap -sn $target" "$target" ;;
+        2)  read -p "Enter target: " target; run_scan "nmap -sS $target" "$target" ;;
+        3)  read -p "Enter target: " target; run_scan "nmap -sU $target" "$target" ;;
+        4)  read -p "Enter target: " target; run_scan "nmap -O $target" "$target" ;;
+        5)  read -p "Enter target: " target; run_scan "nmap -sV $target" "$target" ;;
+        6)  read -p "Enter target: " target; run_scan "nmap -A $target" "$target" ;;
+        7)  read -p "Enter target: " target; run_scan "nmap -F $target" "$target" ;;
+        8)  read -p "Enter target: " target; run_scan "nmap -p- $target" "$target" ;;
+        9)  read -p "Enter target: " target; run_scan "nmap --top-ports 100 $target" "$target" ;;
+        10) read -p "Enter target: " target; run_scan "nmap -f --data-length 200 $target" "$target" ;;
+        11) read -p "Enter target: " target; run_scan "nmap --script vuln $target" "$target" ;;
+        12) read -p "Enter target: " target; run_scan "nmap --script default,vuln $target" "$target" ;;
+        13) read -p "Enter target: " target; read -p "Enter decoy IP (or RND:10): " decoy; run_scan "nmap -D $decoy $target" "$target" ;;
+        14) read -p "Enter target: " target; run_scan "nmap --traceroute $target" "$target" ;;
+        15) read -p "Enter target: " target; read -p "Enter ports (e.g. 21,22,80): " ports; run_scan "nmap -p $ports $target" "$target" ;;
+        16) read -p "Enter target: " target; run_scan "nmap -A $target" "$target" ;;
+        17) read -p "Enter target: " target; read -p "Enter MAC type (e.g. 0=random): " mac; run_scan "nmap --spoof-mac $mac $target" "$target" ;;
+        18) read -p "Enter domain: " target; run_scan "nmap --script dns-brute $target" "$target" ;;
+        19) read -p "Enter target: " target; run_scan "nmap --script http-title $target" "$target" ;;
+        20) read -p "Enter target: " target; run_scan "nmap --script smb-enum-shares,smb-enum-users $target" "$target" ;;
+        21) read -p "Enter target: " target; run_scan "nmap --script ftp-anon $target" "$target" ;;
+        22) read -p "Enter target: " target; run_scan "nmap --script ssl-cert $target" "$target" ;;
+        23) read -p "Enter target: " target; run_scan "nmap --script http-waf-detect $target" "$target" ;;
+        24) read -p "Enter target: " target; run_scan "nmap --script http-methods $target" "$target" ;;
+        25) read -p "Enter target: " target; run_scan "nmap --script http-robots.txt $target" "$target" ;;
+        26) read -p "Enter target: " target; run_scan "nmap --script whois $target" "$target" ;;
+        27) read -p "Enter target: " target; run_scan "nmap --script safe $target" "$target" ;;
+        28) echo -e "\n\e[1;31mExiting Ultra Scan... Goodbye!\e[0m"; exit 0 ;;
+        *) echo -e "\e[1;31mInvalid option. Try again.\e[0m" ;;
     esac
 
     echo -e "\nPress ENTER to return to menu..."
     read
+
+    clear
+
 done
 
